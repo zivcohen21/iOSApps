3b0561b54fd613150f5689d92abec4a7c618f98b
Ziv Cohen <zivcohen21@gmail.com>
2018-08-19 10:00:07 +0300

    iOS apps

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Guest User on 15/08/2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        randomDiceIndex2 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+    }
+}
+
---
+++ Quizzler-iOS11-master/Quizzler/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        checkAnswer()
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        let correctAnswer = allQuestions.list[0].answer
+        
+        if correctAnswer == pickedAnswer {
+            
+        }
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
